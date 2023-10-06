c538a511d798df55013fd5d288512b4cb18df850
Reese <arter.charise@gmail.com>
2023-10-06 10:37:02 -0700

    initial search functionality added

--- src/App.js
+++ src/App.js
@@ -14,20 +14,35 @@ class App extends Component {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((res) => res.json())
 			.then((kittens) => {
-				this.setState(
-					() => {
-						return { kittens };
-					},
-					() => {
-						console.log(this.state);
-					}
-				);
+				this.setState(() => {
+					return { kittens };
+				});
 			});
 	}
 	render() {
+		const { kittens } = this.state;
 		return (
 			<div className='App'>
 				<h1>Hi</h1>
+				<input
+					className='search-box'
+					type='text'
+					placeholder='Search Kittens'
+					onChange={(e) => {
+						const searchField = e.target.value.toLocaleLowerCase();
+
+						const filteredKittens = kittens.filter((kitty) => {
+							return kitty.name.toLowerCase().includes(searchField);
+            })
+            
+            this.setState(() => {
+              return { kittens: filteredKittens }
+            })
+					}}
+				/>
+				{kittens.map((kitty) => {
+					return <h2 key={kitty.id}> {kitty.name} </h2>;
+				})}
 			</div>
 		);
 	}
