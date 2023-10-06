a4649b3a6845a5723b3eb12204867071f65c2f5e
Reese <arter.charise@gmail.com>
2023-10-06 15:37:06 -0700

    cardLIst made into functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,14 @@
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-	render() {
-		const { kittens, className } = this.props;
-		return (
-			<div className={className}>
-				{kittens.map((kitty) => {
-					return <Card key={kitty.id} kitty={kitty} />;
-				})}
-			</div>
-		);
-	}
-}
+const CardList = ({ kittens, className }) => {
+	return (
+		<div className={className}>
+			{kittens.map((kitty) => {
+				return <Card key={kitty.id} kitty={kitty} />;
+			})}
+		</div>
+	);
+};
 
 export default CardList;
