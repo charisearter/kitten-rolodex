c495608877ea372b9d6f7499e32daa5cbfb3a115
Reese <arter.charise@gmail.com>
2023-10-06 15:33:41 -0700

    header component made functional

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,15 +1,11 @@
-import { Component } from 'react';
 import './header.styles.css';
 
-class Header extends Component {
-	render() {
-		const { title, className } = this.props;
-		return (
-			<header>
-				<h1 className={className}> {title} </h1>
-			</header>
-		);
-	}
-}
+const Header = ({ title, className }) => {
+	return (
+		<header>
+			<h1 className={className}> {title} </h1>
+		</header>
+	);
+};
 
 export default Header;
