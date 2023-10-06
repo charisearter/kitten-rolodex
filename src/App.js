3575331038903ab190322c2425ed3983841b49b0
Reese <arter.charise@gmail.com>
2023-10-06 10:30:03 -0700

    componentDidMount data from api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			kittens: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((res) => res.json())
+			.then((kittens) => {
+				this.setState(
+					() => {
+						return { kittens };
+					},
+					() => {
+						console.log(this.state);
+					}
+				);
+			});
+	}
 	render() {
 		return (
 			<div className='App'>
