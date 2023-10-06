d5bdbe020b0d87bc1b47ebd7d91aa4700ae30551
Reese <arter.charise@gmail.com>
2023-10-06 15:35:23 -0700

    card component made functional

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,24 +1,20 @@
-import { Component } from 'react';
 import './card.styles.css';
 
-class Card extends Component {
-	render() {
-		const { name, email, id } = this.props.kitty;
-		return (
-			<div className='card-container'>
-				<div className='card'>
-					<img
-						src={`https://robohash.org/${id}?set=set4&size=180x180`}
-						alt={`${name} Kitty`}
-					/>
-					<div className='card-content'>
-						<h2>{name}</h2>
-						<p>{email}</p>
-					</div>
+const Card = ({ kitty: { name, email, id } }) => {
+	return (
+		<div className='card-container'>
+			<div className='card'>
+				<img
+					src={`https://robohash.org/${id}?set=set4&size=180x180`}
+					alt={`${name} Kitty`}
+				/>
+				<div className='card-content'>
+					<h2>{name}</h2>
+					<p>{email}</p>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Card;
